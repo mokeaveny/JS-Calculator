bc37cc869c1b5d57e29d99025f32599edbaac4ac
mokeaveny <michaelkeaveny0@gmail.com>
2020-04-07 13:51:17 +0100

    Added event listener to all of the buttons

--- CalculatorJavascript.js
+++ CalculatorJavascript.js
@@ -4,10 +4,42 @@ const container = document.getElementById("container");
 const display = document.getElementById("display");
 const buttons = document.getElementById("buttons");
 
+display.textContent = "0";
+
 container.appendChild(display);
 container.appendChild(buttons);
 
+buttons.addEventListener("click", (e) => {
+	if (e.target.matches('button'))
+	{
+		const key = e.target
+		const action = key.dataset.action
+		console.log(action); // Ouputs undefined if it is a number.
+		console.log(key); // Shows the element that is clicked within the buttons div of the document.
+		
+		if (!action) { // If the action is undefined then output number key!
+			console.log("number key!");
+		}
+		if (action == "add" || action == "subtract" || action == "multiply" || action == "divide") {
+			console.log("operator key!");
+		}
+		if (action == "clear") {
+			console.log("Clear key!")
+		}
+		if (action == "calculate") {
+			console.log("Calculate key!")
+		}
+	}
+})
 
+
+
+////////////////////////////
+
+function currentDisplay(theButton)
+{
+	alert(theButton.value);
+}
 
 function add(num1, num2)
 {
