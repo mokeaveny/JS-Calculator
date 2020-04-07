1e0e1605452cc71ce6bd46275e3a2f7ebb910981
mokeaveny <michaelkeaveny0@gmail.com>
2020-04-07 19:31:03 +0100

    Implemented button functionality. Can sucessfully add, divide, multiply, etc... Also added some code to handle unexpected inputs/exceptions. Can't divide by zero and can't perform an operation on a number without another number. For example, input 1 then + then = would just display 1 and you would have to perform the operation on one again.

--- CalculatorJavascript.js
+++ CalculatorJavascript.js
@@ -4,7 +4,11 @@ const container = document.getElementById("container");
 const display = document.getElementById("display");
 const buttons = document.getElementById("buttons");
 
-display.textContent = "0";
+display.textContent = "0"; // Display 0 for the default number.
+
+var firstNumber = "" // String 
+var operator = ""
+var secondNumber = ""; // String
 
 container.appendChild(display);
 container.appendChild(buttons);
@@ -12,22 +16,70 @@ container.appendChild(buttons);
 buttons.addEventListener("click", (e) => {
 	if (e.target.matches('button'))
 	{
-		const key = e.target
-		const action = key.dataset.action
+		const clickedButton = e.target
+		const action = clickedButton.dataset.action
+		const buttonContent = clickedButton.textContent
+		const displayedNum = display.textContent
 		console.log(action); // Ouputs undefined if it is a number.
-		console.log(key); // Shows the element that is clicked within the buttons div of the document.
 		
 		if (!action) { // If the action is undefined then output number key!
-			console.log("number key!");
+			if (displayedNum == "0" || display.textContent == "+" || display.textContent == "-" || display.textContent =="*" || display.textContent=="/") {
+				display.textContent = buttonContent // If current display string is just "0" then we know we can start a number.
+			}
+			else {
+				display.textContent = displayedNum + buttonContent // Append the button clicked to the currentNumber.
+			}
 		}
+
 		if (action == "add" || action == "subtract" || action == "multiply" || action == "divide") {
-			console.log("operator key!");
+			if (firstNumber == "")
+			{
+				firstNumber = display.textContent;
+				console.log(firstNumber);
+			}
+			display.textContent = buttonContent;
+			operator = clickedButton.textContent;
+			console.log(operator);
 		}
 		if (action == "clear") {
-			console.log("Clear key!")
+			firstNumber = ""
+			secondNumber = ""
+			operator = ""
+			display.textContent = "0"
 		}
 		if (action == "calculate") {
-			console.log("Calculate key!")
+			if (firstNumber == "")
+			{
+				display.textContent = firstNumber;
+			}
+			else
+			{
+				firstNumber = Number(firstNumber) // Converts the firstNumber string to an integer value to be used in operation functions.
+				secondNumber = display.textContent;
+				secondNumber = Number(secondNumber) // Converts the secondNumber string to an integer value to be used in operation functions.
+				if (isNaN(secondNumber))
+				{
+					firstNumber = firstNumber.toString() // Convert the firstNumber back to a string
+					operator = "" // Reset operator
+					secondNumber = "" // Reset secondNumber
+					display.textContent = firstNumber;
+				}
+				else
+				{
+					result = operate(operator, firstNumber, secondNumber);
+					if (isNaN(result)) // If the result is Nan ( 0 / 0 ) then display UNDEFINED
+					{
+						display.textContent = "UNDEFINED";
+					}
+					else
+					{
+						display.textContent = result;
+						firstNumber = result.toString(); // Store the result as the firstNumber
+						operator = "" // Reset the operator
+						secondNumber = "" // Reset the secondNumber
+					}
+				}	
+			}
 		}
 	}
 })
